c041d3bb1f5ecaeb9ef3ff68b5fa174052a3092f
eoinfennessy <fennessyeoin@gmail.com>
2022-10-18 23:43:52 +0100

    Improve formatting

--- menu.sh
+++ menu.sh
@@ -1,40 +1,47 @@
 #!/bin/bash
 # Author: Eoin Fennessy
 
+source ./utils/colours.sh
+
+./utils/print_logo.sh
+sleep 0.1
+
 # Displays all select options in one column
 COLUMNS=0
-PS3='Please enter your choice: '
-select opt in "View All Records" \
-              "Add Record" \
-              "Search Records" \
-              "Remove Record(s)" \
-              "Email Householder(s)" \
-              "Exit"; do
-  case $opt in
-    "View All Records")
-      cat ber_details.csv | ./utils/display_records_table.sh
-      ;;
-    "Add Record")
-      ./add.sh
-      ;;
-    "Search Records")
-      ./search/search_menu.sh
-      ;;
-    "Remove Record(s)")
-      ./remove.sh
-      ;;
-    "Email Householder(s)")
-      ./Email.sh
-      ;;
-    "Exit")
-      echo "Goodbye!"
-      sleep 0
-      break
-      ;;
-    *) 
-      echo "$REPLY is not a valid option"
-      ;;
-  esac
-  # Setting REPLY to an empty string causes user options to be redisplayed
-  REPLY=""
+menu_options=("View All Records" "Add Record" "Search Records"
+              "Remove Record(s)" "Email Householder(s)" "Exit")
+
+while true; do
+    clear
+    echo -e "${COL_HEAD}            Main Menu            ${COL_OFF}"
+    PS3=$"${COL_PROMPT_PS3}Please enter your choice (1-6): ${COL_OFF_PS3}"
+    select option in "${menu_options[@]}"; do
+        case $option in
+        "View All Records")
+            ./utils/display_records_table.sh < ./ber_details.csv | less
+            ;;
+        "Add Record")
+            ./add.sh
+            ;;
+        "Search Records")
+            ./search/search_menu.sh
+            ;;
+        "Remove Record(s)")
+            ./remove.sh
+            ;;
+        "Email Householder(s)")
+            ./Email.sh
+            ;;
+        "Exit")
+            echo "Goodbye!"
+            sleep 0
+            exit 0
+            ;;
+        *) 
+            echo -e "${COL_ERR}${REPLY} is not a valid option${COL_OFF}"
+            sleep 1.5
+            ;;
+        esac
+        break
+    done
 done
