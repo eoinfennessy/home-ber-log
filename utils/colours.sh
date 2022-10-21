32dfa0bed0206d9d256c377e22ea668a2d6a7f7c
eoinfennessy <fennessyeoin@gmail.com>
2022-10-21 22:12:27 +0100

    Add colour to logo

--- utils/colours.sh
+++ utils/colours.sh
@@ -1,12 +1,26 @@
+#!/bin/bash
+# Author: Eoin Fennessy
+# Contains colour constants for app
+
 # Reset
 COL_OFF='\033[0m'
 
 COL_HEAD='\033[44m'
-COL_PROMPT='\033[38;05;40m'
+COL_PROMPT='\033[38;05;42m'
 COL_MSG='\033[38;05;5m'
 COL_WARN='\033[38;05;220m'
 COL_ERR='\033[38;05;202m'
 
+# Logo colours
+COL_BOLD_WHITE='\033[1;37m'
+COL_LOGO_A='\033[38;2;44;101;22m'
+COL_LOGO_B='\033[38;2;88;153;64m'
+COL_LOGO_C='\033[38;2;132;186;108m'
+COL_LOGO_D='\033[38;2;254;255;11m'
+COL_LOGO_E='\033[38;2;250;190;7m'
+COL_LOGO_F='\033[38;2;250;135;7m'
+COL_LOGO_G='\033[38;2;250;67;6m'
+
 # For use with PS3 prompt only
 COL_OFF_PS3=$'\001\e[0m\002'
-COL_PROMPT_PS3=$'\001\e[38;05;42m\002' #77, 79, 10, 42
+COL_PROMPT_PS3=$'\001\e[38;05;42m\002'
---
+++ utils/print_logo.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Author: Eoin Fennessy
+
+source ./utils/colours.sh
+
+echo -e "${COL_LOGO_A}■■◣${COL_BOLD_WHITE}                           BBBBBBBBBBBBBBBBB    EEEEEEEEEEEEEEEEEEEEEE RRRRRRRRRRRRRRRRR"
+echo -e "${COL_LOGO_A}■■◤${COL_BOLD_WHITE}                           B::::::::::::::::B   E::::::::::::::::::::E R::::::::::::::::R"
+echo -e "${COL_LOGO_A}■■■■■◣${COL_BOLD_WHITE}                        B::::::BBBBBB:::::B  E::::::::::::::::::::E R::::::RRRRRR:::::R"
+echo -e "${COL_LOGO_A}■■■■■◤${COL_BOLD_WHITE}                        BB:::::B     B:::::B EE::::::EEEEEEEEE::::E RR:::::R     R:::::R"
+echo -e "${COL_LOGO_B}■■■■■■■■◣${COL_BOLD_WHITE}                       B::::B     B:::::B   E:::::E       EEEEEE   R::::R     R:::::R"
+echo -e "${COL_LOGO_B}■■■■■■■■◤${COL_BOLD_WHITE}                       B::::B     B:::::B   E:::::E                R::::R     R:::::R"
+echo -e "${COL_LOGO_C}■■■■■■■■■■■◣${COL_BOLD_WHITE}                    B::::BBBBBB:::::B    E::::::EEEEEEEEEE      R::::RRRRRR:::::R"
+echo -e "${COL_LOGO_C}■■■■■■■■■■■◤${COL_BOLD_WHITE}                    B:::::::::::::BB     E:::::::::::::::E      R:::::::::::::RR"
+echo -e "${COL_LOGO_D}■■■■■■■■■■■■■■◣${COL_BOLD_WHITE}                 B::::BBBBBB:::::B    E:::::::::::::::E      R::::RRRRRR:::::R"
+echo -e "${COL_LOGO_D}■■■■■■■■■■■■■■◤${COL_BOLD_WHITE}                 B::::B     B:::::B   E::::::EEEEEEEEEE      R::::R     R:::::R"
+echo -e "${COL_LOGO_E}■■■■■■■■■■■■■■■■■◣${COL_BOLD_WHITE}              B::::B     B:::::B   E:::::E                R::::R     R:::::R"
+echo -e "${COL_LOGO_E}■■■■■■■■■■■■■■■■■◤${COL_BOLD_WHITE}              B::::B     B:::::B   E:::::E       EEEEEE   R::::R     R:::::R"
+echo -e "${COL_LOGO_F}■■■■■■■■■■■■■■■■■■■■◣${COL_BOLD_WHITE}         BB:::::BBBBBB::::::B EE::::::EEEEEEEE:::::E RR:::::R     R:::::R"
+echo -e "${COL_LOGO_F}■■■■■■■■■■■■■■■■■■■■◤${COL_BOLD_WHITE}         B:::::::::::::::::B  E::::::::::::::::::::E R::::::R     R:::::R"
+echo -e "${COL_LOGO_G}■■■■■■■■■■■■■■■■■■■■■■■◣${COL_BOLD_WHITE}      B::::::::::::::::B   E::::::::::::::::::::E R::::::R     R:::::R"
+echo -e "${COL_LOGO_G}■■■■■■■■■■■■■■■■■■■■■■■◤${COL_BOLD_WHITE}      BBBBBBBBBBBBBBBBB    EEEEEEEEEEEEEEEEEEEEEE RRRRRRRR     RRRRRRR${COL_OFF}"
